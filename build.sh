07c178d04841fea27fef40ba480c6f27a9bb920e
Gary Baugh <baughg@tcd.ie>
2017-10-03 21:04:09 +0100

    hilbert curves added

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm decoder
-g++ -O2 -std=c++11 video_frame.cpp frame_server.cpp histogram.cpp rgb_histogram.cpp decoder.cpp -lavformat -lavcodec -lswscale -lavutil -lavfilter -lswresample -lavdevice -lpostproc -lx264 -lrt -o decoder
+g++ -O2 -std=c++11 hilbert_curve2d.cpp video_frame.cpp frame_server.cpp histogram.cpp rgb_histogram.cpp decoder.cpp -lavformat -lavcodec -lswscale -lavutil -lavfilter -lswresample -lavdevice -lpostproc -lx264 -lrt -o decoder
