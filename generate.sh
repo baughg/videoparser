fb786cb0d2c36925e11ef24300f0fea39b12d982
Gary Baugh <baughg@tcd.ie>
2017-12-18 01:09:19 +0000

    update script

--- generate.sh
+++ generate.sh
@@ -3,6 +3,7 @@ rm -rf dump
 rm hist.dat
 rm *.bmp
 rm frame_rank.dat
+rm face_detection.sh
 echo running $1
 vdecoder hist $1
 vdecoder rank $1
@@ -11,3 +12,6 @@ vdecoder thumb $1
 vdecoder face $1
 chmod +x face_detection.sh
 ./face_detection.sh
+rm -rf face
+mkdir face
+vdecoder facepull $1
