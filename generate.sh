51953705d2eb2abad8bd055415d3bafb887438a0
Gary Baugh <baughg@tcd.ie>
2017-12-19 15:16:05 +0000

    update script

--- generate.sh
+++ generate.sh
@@ -2,8 +2,10 @@
 rm -rf dump
 rm -rf dumpt
 rm -rf face
+rm -rf facesel
 rm hist.dat
 rm *.bmp
+rm *.png
 rm frame_rank.dat
 rm frame_rank_thumb.dat
 rm face_detection.sh
@@ -17,4 +19,11 @@ vdecoder face $1
 chmod +x face_detection.sh
 ./face_detection.sh
 mkdir face
+mkdir facesel
 vdecoder facepull $1
+vdecoder facesel $1
+chmod +x face_resize.sh
+./face_resize.sh
+vdecoder facealbum $1
+chmod +x face_album.sh
+./face_album.sh
